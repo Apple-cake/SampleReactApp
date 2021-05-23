20eb81152f8476f190c23c86b1458b89a0a4f132
Apple-cake <tongensai.kudasai@gmail.com>
2021-05-24 03:24:32 +0900

    constructor追加

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super()
+    this.title = props.title
+    this.message = props.message
+  }
+
   render() {
     return <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="subtitle">This is sample component.</p>
+        <p className="subtitle">{this.title}</p>
         <p>これはサンプルのコンポーネントです。</p>
-        <p>簡単なメッセージを表示します。</p>
+        <p>{this.message}</p>
       </div>
     </div>
   }
